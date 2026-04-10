f08342ab5249219998cd6b3f1e435d608d341ab8
doriangoobie <dorian.goobie@student.tdsb.on.ca>
2026-04-10 13:46:52 +0000

    submit assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,21 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sumOfDigits(int x) {
+		x = Math.abs(x);
+		if (x == 0) {
+			return 0;
+		}
+	return x%10 + sumOfDigits(x/10);
 	}
 
+	public static int factorial(int x) {
+	if (x == 0) {
+		return 1;
+	}
+	if (x < 0) {
+		return 0;
+	}
+	return x*factorial(x - 1);
+	}
+}		
 	
-
-}
